fa15cfd6d5c25dff48de485e948fb4a5eaa99083
Luis Henrique <119861034+L-hso@users.noreply.github.com>
2024-11-26 23:01:27 +0000

    Implementation of dropdown in the actions area and abstraction of dropdown creation for the messages area as contextmenu and via button

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 import { Dropdown } from "./utils.js";
-import Conversation  from "./conversation.js";
+import Conversation from "./conversation.js";
 
 const messagesMain = document.querySelector("#messages main ul");
 
-const conversationsData = await fetch("../assets/fakedata.json").then(response=>response.json());
+const conversationsData = await fetch("../assets/fakedata.json").then(
+  (response) => response.json()
+);
 
-await conversationsData.forEach((data,) => {
+await conversationsData.forEach((data) => {
   messagesMain.appendChild(Conversation.create(data));
 });
 
@@ -13,49 +15,43 @@ const conversations = document.querySelectorAll(
   "#messages main .conversation_block"
 );
 
+
+function conversationsCreateContextMenu(e, {x=e.clientX, y=e.clientY, marginX, marginY}){
+  const popover = document.querySelector(".conversation_options_popover");
+
+  e.stopImmediatePropagation();
+  e.preventDefault();
+
+  if (popover == null) {
+    let blockPos = {x, y, marginX, marginY};
+
+    const menu = Dropdown.create(
+      e.target.dataset.id,
+      [
+        "Arquivar conversa",
+        "Silenciar notificações",
+        "Apagar conversa",
+        "Fixar conversa",
+        "Marcar como não lida",
+        "Adicionar aos favoritos",
+        "Bloquear",
+      ],
+      blockPos
+    );
+
+    document.body.appendChild(menu);
+  }
+}
+
+
 conversations.forEach((block) => {
   const conversationOptions = block.querySelector(".conversation_options");
 
-  conversationOptions.addEventListener("click", (e) => {
-    const popover = document.querySelector(".conversation_options_popover");
-
-    e.stopImmediatePropagation();
-    if (popover != null) {
-      popover.animate([{ transform: "scale(0)", opacity: 0 }], {
-        duration: 340,
-        easing: "ease",
-      });
-
-      setTimeout(() => {
-        document.body.removeChild(popover);
-      }, 330);
-    } else {
-      
-      let blockPos = {
-        x: conversationOptions.getBoundingClientRect().x.toString(),
-        y: conversationOptions.getBoundingClientRect().y.toString(),
-      };
-
-      const menu = Dropdown.create(
-        block.dataset.id,
-        [
-          "Arquivar conversa",
-          "Silenciar notificações",
-          "Apagar conversa",
-          "Fixar conversa",
-          "Marcar como não lida",
-          "Adicionar aos favoritos",
-          "Bloquear",
-        ],
-        blockPos
-      );
-
-      document.body.appendChild(menu);
-    }
-  });
+  conversationOptions.addEventListener("click", (e)=>{conversationsCreateContextMenu(e, {x: e.target.getBoundingClientRect().x, y: e.target.getBoundingClientRect().y, marginX: "1.5", marginY: "0"})});
+  block.addEventListener("contextmenu", (e)=>{conversationsCreateContextMenu(e, {marginX: "0", marginY: "0"})});
 });
 
-document.body.addEventListener("click", () => {
+document.body.addEventListener("mousedown", () => {
   const popover = document.querySelector(".conversation_options_popover");
 
   if (popover != null) {
@@ -65,7 +61,7 @@ document.body.addEventListener("click", () => {
     });
 
     setTimeout(() => {
-      document.body.removeChild(popover);
+      popover.remove();
     }, 330);
   }
 });
@@ -73,3 +69,44 @@ document.body.addEventListener("click", () => {
 const navigationBar = document.querySelector("nav");
 
 const messagesArea = document.querySelector("#messages");
+
+const principalPanel = document.querySelector("#principal_panel");
+
+principalPanel.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  if (!document.querySelector(".conversation_options_popover")) {
+    let options;
+
+    if (e.target.dataset.chattype == "group") {
+      options = [
+        "Dados do grupo",
+        "Selecionar mensagens",
+        "Fechar conversa",
+        "Silenciar notificações",
+        "Mensagens temporárias",
+        "Limpar conversa",
+        "Sair do grupo",
+        "Adicionar aos favoritos",
+      ];
+    }
+
+    if (e.target.dataset.chattype == "person") {
+      options = [
+        "Dados do contato",
+        "Selecionar mensagens",
+        "Fechar conversa",
+        "Silenciar notificações",
+        "Mensagens temporárias",
+        "Limpar conversa",
+        "Apagar conversa",
+        "Adicionar aos favoritos",
+        "Denunciar",
+        "Bloquear",
+      ];
+    }
+
+    const dropdown = Dropdown.create(-1, options, {x: e.clientX, y: e.clientY, marginX: "0", marginY: "0"});
+
+    document.body.appendChild(dropdown);
+  }
+});
