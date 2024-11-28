eb396844bc1a4e7f9aa5a62ad69f1046e3bc5c16
Luis Henrique <119861034+L-hso@users.noreply.github.com>
2024-11-28 00:09:56 +0000

    Principal panel implementation, nochat done missing group and person panels

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,20 @@
 import { Dropdown } from "./utils.js";
 import Conversation from "./conversation.js";
-
-const messagesMain = document.querySelector("#messages main ul");
-
-const conversationsData = await fetch("../assets/fakedata.json").then(
-  (response) => response.json()
-);
-
-await conversationsData.forEach((data) => {
-  messagesMain.appendChild(Conversation.create(data));
-});
-
-const conversations = document.querySelectorAll(
-  "#messages main .conversation_block"
-);
+import PrincipalPanel from "./principal_panel.js";
 
 
-function conversationsCreateContextMenu(e, {x=e.clientX, y=e.clientY, marginX, marginY}){
+/*---------------------- CONVERSATIONS AREA ----------------------*/
+function conversationsCreateContextMenu(
+  e,
+  { x = e.clientX, y = e.clientY, marginX, marginY }
+) {
   const popover = document.querySelector(".conversation_options_popover");
 
   e.stopImmediatePropagation();
   e.preventDefault();
 
   if (popover == null) {
-    let blockPos = {x, y, marginX, marginY};
+    let blockPos = { x, y, marginX, marginY };
 
     const menu = Dropdown.create(
       e.target.dataset.id,
@@ -44,40 +35,89 @@ function conversationsCreateContextMenu(e, {x=e.clientX, y=e.clientY, marginX, m
 }
 
 
+const messagesMain = document.querySelector("#messages main ul");
+
+const conversationsData = await fetch("../assets/fakedata.json").then(
+  (response) => response.json()
+);
+
+await conversationsData.forEach((data) => {
+  messagesMain.appendChild(Conversation.create(data));
+});
+
+const conversations = document.querySelectorAll(
+  "#messages main .conversation_block"
+);
+
 conversations.forEach((block) => {
   const conversationOptions = block.querySelector(".conversation_options");
 
-  conversationOptions.addEventListener("click", (e)=>{conversationsCreateContextMenu(e, {x: e.target.getBoundingClientRect().x, y: e.target.getBoundingClientRect().y, marginX: "1.5", marginY: "1.5"})});
-  block.addEventListener("contextmenu", (e)=>{conversationsCreateContextMenu(e, {marginX: "0", marginY: "0"})});
-});
-
-document.body.addEventListener("mousedown", () => {
-  const popover = document.querySelector(".conversation_options_popover");
-
-  if (popover != null) {
-    popover.animate([{ transform: "scale(0)", opacity: 0 }], {
-      duration: 340,
-      easing: "ease",
+  conversationOptions.addEventListener("click", (e) => {
+    conversationsCreateContextMenu(e, {
+      x: e.target.getBoundingClientRect().x,
+      y: e.target.getBoundingClientRect().y,
+      marginX: "1.5",
+      marginY: "1.5",
     });
-
-    setTimeout(() => {
-      popover.remove();
-    }, 330);
-  }
+  });
+  block.addEventListener("contextmenu", (e) => {
+    conversationsCreateContextMenu(e, { marginX: "0", marginY: "0" });
+  });
 });
+/*-----------------------------------------------------------*/
+
 
 const navigationBar = document.querySelector("nav");
 
 const messagesArea = document.querySelector("#messages");
 
+
+/*---------------------- PRINCIPAL PANEL AREA ----------------------*/
+
 const principalPanel = document.querySelector("#principal_panel");
+
+principalPanel.appendChild(PrincipalPanel.createNochatPanel());
+
+const principalPanelMutationObserver = new MutationObserver((mutationList) => {
+  for (const mutation of mutationList) {
+    if (mutation.target.dataset.chatId == null) return;
+
+    if (mutation.type === "attributes") {
+      mutation.target.childNodes[0].remove();
+
+      if (mutation.target.dataset.chattype == "nochat") {
+        mutation.target.appendChild(PrincipalPanel.createNochatPanel());
+        return;
+      }
+
+      if (mutation.target.dataset.chattype == "group") {
+        mutation.target.appendChild(PrincipalPanel.createGroupPanel());
+        return;
+      }
+
+      if (mutation.target.dataset.chattype == "person") {
+        mutation.target.appendChild(PrincipalPanel.createPersonPanel());
+        return;
+      }
+    }
+  }
+});
+
+principalPanelMutationObserver.observe(principalPanel, {
+  attributes: true,
+  attributeFilter: ["data-chattype"],
+});
 
 principalPanel.addEventListener("contextmenu", (e) => {
   e.preventDefault();
-  if (!document.querySelector(".conversation_options_popover")) {
+
+  if (
+    !document.querySelector(".conversation_options_popover") &&
+    e.currentTarget.dataset.chattype != "nochat"
+  ) {
     let options;
 
-    if (e.target.dataset.chattype == "group") {
+    if (e.currentTarget.dataset.chattype == "group") {
       options = [
         "Dados do grupo",
         "Selecionar mensagens",
@@ -90,7 +130,7 @@ principalPanel.addEventListener("contextmenu", (e) => {
       ];
     }
 
-    if (e.target.dataset.chattype == "person") {
+    if (e.currentTarget.dataset.chattype == "person") {
       options = [
         "Dados do contato",
         "Selecionar mensagens",
@@ -105,8 +145,30 @@ principalPanel.addEventListener("contextmenu", (e) => {
       ];
     }
 
-    const dropdown = Dropdown.create(-1, options, {x: e.clientX, y: e.clientY, marginX: "0", marginY: "0"});
+    const dropdown = Dropdown.create(-1, options, {
+      x: e.clientX,
+      y: e.clientY,
+      marginX: "0",
+      marginY: "0",
+    });
 
     document.body.appendChild(dropdown);
+  }
+});
+
+/*-----------------------------------------------------------*/
+
+document.body.addEventListener("mousedown", () => {
+  const popover = document.querySelector(".conversation_options_popover");
+
+  if (popover != null) {
+    popover.animate([{ transform: "scale(0)", opacity: 0 }], {
+      duration: 340,
+      easing: "ease",
+    });
+
+    setTimeout(() => {
+      popover.remove();
+    }, 330);
   }
 });
